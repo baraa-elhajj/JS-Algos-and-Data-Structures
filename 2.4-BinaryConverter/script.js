af54bd25a212a2f50dc6c19a913c4f91853b0bce
Baraa El Hajj <baraa.w.hajj@gmail.com>
2024-12-17 23:49:48 +0200

    Update script.js
    Changed decimalToBinary function to recursive

--- 2.4-BinaryConverter/script.js
+++ 2.4-BinaryConverter/script.js
@@ -2,19 +2,29 @@ const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
+
+
+// const decimalToBinary = (input) => {
+//     let binary = "";
+
+//     if (input === 0) {
+//         binary = "0";
+//     }
+
+//     while (input > 0) {
+//         binary = (input % 2) + binary;
+//         input = Math.floor(input / 2);
+//     }
+
+//     result.innerText = binary;
+// };
+
 const decimalToBinary = (input) => {
-    let binary = "";
-
-    if (input === 0) {
-        binary = "0";
+    if (input === 0 || input === 1) {
+        return String(input);
+    } else {
+        return decimalToBinary(Math.floor(input / 2)) + (input % 2);
     }
-
-    while (input > 0) {
-        binary = (input % 2) + binary;
-        input = Math.floor(input / 2);
-    }
-
-    result.innerText = binary;
 };
 
 const checkUserInput = () => {
@@ -27,7 +37,7 @@ const checkUserInput = () => {
         return;
     }
 
-    decimalToBinary(parseInt(numberInput.value));
+    result.textContent = decimalToBinary(parseInt(numberInput.value));
     numberInput.value = "";
 };
 
