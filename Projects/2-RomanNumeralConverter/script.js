9bffb032fe0eea3a11fa47d4633d685adfa14210
Baraa El Hajj <baraa.w.hajj@gmail.com>
2024-12-18 14:23:26 +0200

    Update script.js
    Added number check and event listeners

--- Projects/2-RomanNumeralConverter/script.js
+++ Projects/2-RomanNumeralConverter/script.js
@@ -1,1 +1,29 @@
-// Logic goes here
+// DOM Variables
+const numberInput = document.getElementById("number-input");
+const convertBtn = document.getElementById("convert-button");
+const result = document.getElementById("result-container");
+
+// Methodes
+const numberToRoman = (number) => {
+    // Roman conversion logic goes here
+}
+
+const checkUserInput = () => {
+    const inputInt = parseInt(numberInput.value);
+
+    if (!numberInput.value || isNaN(inputInt) || inputInt <= 0 || inputInt >= 3999) {
+        alert("Please provide a number strictly between 0 and 3999");
+        return;
+    }
+
+    result.textContent = numberToRoman(inputInt);
+};
+
+// Event Listeners
+convertBtn.addEventListener("click", checkUserInput);
+
+numberInput.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+        checkUserInput();
+    }
+});
