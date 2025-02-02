042d941a553456186ca9b2e6819336b61807e875
Baraa El Hajj <baraa.w.hajj@gmail.com>
2025-02-02 20:57:25 +0200

    Update script.js

--- Projects/4-CashRegister/script.js
+++ Projects/4-CashRegister/script.js
@@ -1,14 +1,14 @@
-let price = 3.26;
+let price = 1.87;
 let cid = [
-    ['PENNY', 1.01],
-    ['NICKEL', 2.05],
-    ['DIME', 3.1],
-    ['QUARTER', 4.25],
-    ['ONE', 90],
-    ['FIVE', 55],
-    ['TEN', 20],
-    ['TWENTY', 60],
-    ['ONE HUNDRED', 100]
+    ['PENNY', 101],
+    ['NICKEL', 205],
+    ['DIME', 310],
+    ['QUARTER', 425],
+    ['ONE', 9000],
+    ['FIVE', 5500],
+    ['TEN', 2000],
+    ['TWENTY', 6000],
+    ['ONE HUNDRED', 10000]
 ];
 
 const displayChangeDue = document.getElementById('change-due');
